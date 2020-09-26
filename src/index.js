5a45986d478fe997df9a12546ec5250503401088
usmanahmed-m <usman-malik@outlook.com>
2020-09-27 02:55:29 +0500

    created assignment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Hi() {
+  return (
+    <div>
+      <h1>Learning React from Dave Ceddia tutorial 2020</h1>
+      <br />
+      Hello World from <strong>Usman</strong>
+      <p>JavaScript Expression: 5 + 10 = {5 + 10}</p>
+    </div>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Hi />, document.querySelector("#root"));
